dd839759d2fc6e50fcdad8a132daf0548fa9ec0a
francescofrigerio <francesco.frigerio@greencore.it>
2021-12-14 09:05:06 +0100

    Lezione 183 Sostituzione Junit4 con Junit5 Nei files pom.xml(data e web) e MainApplTest

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+// LEZIONE 183 Sostituzione di Junit4 con Junit5
+// Vedere modifica al file pom.xml dei 2 moduli
+/* @RunWith(SpringRunner.class) */
+// Rimane un test d'integrazione qundi
+// molto lento perche' viene caricato lo Spring Context
+// LEZIONE 183 Sostituzione di Junit4 con Junit5
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
