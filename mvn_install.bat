08fe80cf0886474a64ac95cf2cf92243334534b7
francescofrigerio <francesco.frigerio@greencore.it>
2021-12-16 09:18:43 +0100

    Lezione 188 Modifica al file config.yml di CircleCi

--- mvn_install.bat
+++ mvn_install.bat
@@ -1,5 +1,5 @@
-rem SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
-SET JAVA_HOME=C:\Program Files\Java\jdk-12.0.1
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
+REM SET JAVA_HOME=C:\Program Files\Java\jdk-12.0.1
 
 rem mvn verify >verify.log 2>&1
 
